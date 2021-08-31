f7e9cebfc6c73a1e390cd413c71834245122e27b
Teemu Laurila <laurilateemu8@gmail.com>
2021-08-31 13:39:45 +0300

    Add faster version for streaming from Linux

--- 1_flaskStream_opencv.py
+++ 1_flaskStream_opencv.py
@@ -2,44 +2,84 @@ from flask import Flask, render_template, Response
 import cv2
 import pyautogui
 import numpy as np
+import time
+import mss
+import simplejpeg
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
+#camera = cv2.VideoCapture(0)  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            #ret, buffer = cv2.imencode('.jpg', frame)
+new_frame_time = 0
+prev_frame_time = 0
 
-            scale = 1/16
+def gen_frames():  # generate frame by frame from camera
+    global new_frame_time
+    global prev_frame_time
+
+    while True:
+        with mss.mss() as sct:
+            monitor = sct.monitors[1]
+            # Capture frame-by-frame
+            #success, frame = camera.read()  # read the camera frame
+            #if not success:
+            #    break
+            #else:
+                #ret, buffer = cv2.imencode('.jpg', frame)
+
+            #scale = 1/16
 
             # PYAUTOGUI
-            img = pyautogui.screenshot()
-            img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            #img = pyautogui.screenshot()
+            img = np.array(sct.grab(monitor))
+            #img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
             #img = cv2.resize(img, ((int(2560*scale),int(1440*scale))))
 
             # HIGH RES BUT SLOW?
             #img = cv2.resize(img, ((480,320)), interpolation=cv2.INTER_LANCZOS4)
-            img = cv2.resize(img, ((240,180)), interpolation=cv2.INTER_LANCZOS4)
+            #img = cv2.resize(img, ((480,320)), interpolation=cv2.INTER_LINEAR)
 
-            ret, buffer = cv2.imencode('.jpg', img)
+            new_frame_time = time.time()
+
+            # fps will be number of frame processed in given time frame
+            # since their will be most of time error of 0.001 second
+            # we will be subtracting it to get more accurate result
+            fps = 1/(new_frame_time-prev_frame_time)
+            prev_frame_time = new_frame_time
+        
+            # converting the fps into integer
+            fps = int(fps)
+        
+            # converting the fps to string so that we can display it on frame
+            # by using putText function
+            fps = str(fps)
+        
+            # putting the FPS count on the frame
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+            ret, buffer = cv2.imencode('.jpg', img, encode_param)
+            #frame = simplejpeg.encode_jpeg(img)
             
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                    b'Content-Type:image/jpeg\r\n'
+                    b'Content-Length: ' + f"{len(frame)}".encode() + b'\r\n'
+                    b'\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
 @app.route('/video')
 def video():
@@ -49,7 +89,7 @@ def video():
             <title>Video Streaming Demonstration</title>
         </head>
         <body style="background-color:black;">
-            <img id="stream" heigth="100%" width="100%" src="http://0.0.0.0:5000/video_feed" onerror="javascript: alert('failure')"/>
+            <img id="stream" heigth="100%" width="100%" src="http://192.168.253.69:5000/video_feed" onerror="javascript: alert('failure')"/>
         </body>
     
     <script>
